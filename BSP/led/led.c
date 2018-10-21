8e7efb127bc69d8bb330b68068ee6e10532cf48f
zhengxin <43071004+guanjianhe@users.noreply.github.com>
2018-10-21 14:23:14 +0800

    Update led.c

--- BSP/led/led.c
+++ BSP/led/led.c
@@ -1,5 +1,10 @@
 #include "led.h"
 
+
+#define RCC_LED     RCC_APB2Periph_GPIOA
+#define PORT_LED    GPIOA
+#define PIN_LED     GPIO_Pin_8
+
 /* 初始化LED */
 void InitLed( void )
 {
@@ -7,10 +12,10 @@ void InitLed( void )
     GPIO_InitTypeDef GPIO_InitStructure;
 
     /*开启GPIOC外设时钟*/
-    RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA, ENABLE );
+    RCC_APB2PeriphClockCmd( RCC_LED, ENABLE );
 
     /* 选择要控制的GPIOB引脚 */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+    GPIO_InitStructure.GPIO_Pin = PIN_LED;
 
     /*设置引脚速率为50MHz */
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
@@ -19,9 +24,9 @@ void InitLed( void )
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 
     /*调用库函数，初始化GPIOA*/
-    GPIO_Init( GPIOA, &GPIO_InitStructure );
+    GPIO_Init( PORT_LED, &GPIO_InitStructure );
 
     /* 关闭led灯 */
-    GPIO_SetBits( GPIOA, GPIO_Pin_8 );
+    GPIO_SetBits( PORT_LED, PIN_LED );
 }
 
